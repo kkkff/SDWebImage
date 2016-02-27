0480f08e0f93add3509f8cad68161a1d4fa31be9
mo <fdsfs3355@163.com>
2016-02-27 23:22:56 +0800

    pesfsgdf

---
+++ Tests/pesfagdf.h
@@ -0,0 +1,13 @@
+//
+//  pesfagdf.h
+//  SDWebImage Tests
+//
+//  Created by applemo on 16/2/27.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface pesfagdf : UITabBar
+
+@end
---
+++ Tests/pesfagdf.m
@@ -0,0 +1,21 @@
+//
+//  pesfagdf.m
+//  SDWebImage Tests
+//
+//  Created by applemo on 16/2/27.
+//
+//
+
+#import "pesfagdf.h"
+
+@implementation pesfagdf
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
